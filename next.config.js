039e44514c4310f2921cd3be423a4dc1dbb74dde
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2020-03-04 11:39:02 +0800

    change config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = (phase, { defaultConfig }) => {
     return {
         env: {
             BASE_URL: process.env.BASE_URL,
-            mock: process.env.mock
+            mock: process.env.mock === 'true' ? true : false
         }
         /* config options for all phases except development here */
     }
